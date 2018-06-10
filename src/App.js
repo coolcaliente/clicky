fe988766607e75c452150079b8972b604d1c5061
coolcaliente <andreinakeller@gmail.com>
2018-06-10 14:09:58 -0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import Nav from "./components/Nav";
+import './App.css';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+const App = () => (
+  <Nav />
+
+  // <Header />
+
+  // <Main>
+
+  //   <ArrestedDevelopmentCard />
+
+  
+  // </ Main>
+
+);
+
+export default App;
